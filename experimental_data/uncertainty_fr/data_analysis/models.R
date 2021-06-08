ed54e1931eb0d91da61b6b0b30f45e4711a3d60b
nicolasluarte <l.luarte@uc.cl>
2021-06-08 01:40:49 -0400

    initial models

--- experimental_data/uncertainty_fr/data_analysis/models.R
+++ experimental_data/uncertainty_fr/data_analysis/models.R
@@ -1,26 +1,49 @@
-library(tidyverse)
+library(tidymodels)
 library(lme4)
 library(lmerTest)
+library(sjPlot)
+library(sjmisc)
 
 df <- readRDS("data.rds")
 
-# data imputation
+# function to transform date to sessions
+date2s <- function(date.vec) {
+	sort(date.vec) %>%
+		as.numeric() %>%
+		diff() %>%
+		{ if_else(. > 0, 1, 0) } %>%
+		cumsum() -> sessions
+	return(c(1, sessions + 1))
+}
+df %>%
+	mutate(session = date2s(date)) -> df
 
-# model 1: treatment vs control, experimental phase, number of events
-model.0 <- lmer(data = df %>% filter(condition == "post"),
-		sumEvents ~ (1 | raton))
-model.1 <- lmer(data = df %>% filter(condition == "post"),
-		sumEvents ~ expGroup + (1 | raton))
-model.2 <- lmer(data = df %>% filter(condition == "post"),
-		sumLicks ~ expGroup + as.numeric(date) + (1 | raton))
-model.3 <- lmer(data = df %>% filter(condition == "post"),
-		sumEvents ~ expGroup * as.numeric(date) + (expGroup | raton),
-		control = lmerControl(optimizer ="Nelder_Mead"))
+model.0 <- lmer(data = df %>% filter(date >= "2021-05-05"),
+	sumEvents ~ expGroup * condition + (1 | raton))
+summary(model.0)
 
-anova(model.0, model.1, model.2)
+model.1 <- lmer(data = df %>% filter(date >= "2021-05-05"),
+	sumEvents ~ expGroup * condition + (session | raton))
+summary(model.1)
 
+model.2 <- lmer(data = df %>% filter(date >= "2021-05-05"),
+	sumEvents ~ expGroup * condition * session + (1 | raton))
+
+model.3 <- lmer(data = df %>% filter(date >= "2021-05-05"),
+	sumLicks ~ expGroup * condition * session + (session | raton),
+control = lmerControl(optimizer = "Nelder_Mead"))
 
 model.4 <- lmer(data = df %>% filter(date >= "2021-05-05"),
-		sumEvents ~ expGroup * condition + (1 | raton),
-		control = lmerControl(optimizer = "Nelder_Mead"))
-model.4 %>% summary()
+	sumEvents ~ expGroup * condition  + (1 | raton),
+control = lmerControl(optimizer = "Nelder_Mead"))
+
+model.5 <- lmer(data = df %>% filter(date >= "2021-05-05"),
+	sumLicks ~ expGroup * condition  + (1 | raton) + (1 | condition),
+control = lmerControl(optimizer = "Nelder_Mead"))
+
+
+
+plot_model(model.1, type = "pred", terms = c("session", "expGroup", "condition"))
+plot_model(model.3, type = "pred", terms = c("session", "expGroup", "condition"))
+plot_model(model.4, type = "eff", terms = c("expGroup", "condition"))
+plot_model(model.5, type = "eff", terms = c("expGroup", "condition"))
